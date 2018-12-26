631a12150c64e020e070b665ea80e18f3b2797ec
Quentin Gusbin <qg3393@hotmail.com>
2018-12-26 19:21:03 +0100

    add -y

--- ubuntu/install_cuda.sh
+++ ubuntu/install_cuda.sh
@@ -4,4 +4,4 @@
 wget -c "https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb"
 sudo dpkg --install cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
 sudo apt-get update
-sudo apt-get install cuda
+sudo apt-get install -y cuda
